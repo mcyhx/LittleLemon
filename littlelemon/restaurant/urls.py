c5a9af8ca7bba370898e223882ffeb20fdf23dc9
mcyhx <ruolanlin@gmail.com>
2023-06-07 22:28:30 +0800

    complete the capstone project

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
 
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menuitems/', views.MenuItemsView.as_view(), name='menuitems-list'),
+    path('menuitems/<int:pk>/', views.SingleMenuItemView.as_view(), name='menuitems-detail'),
+    path("bookings/",views.BookingListCreateView.as_view(), name="bookings"),
+    path('bookings/<int:pk>/', views.SingleBookingview.as_view(), name='booking-detail'),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer,MenuItemSerializer,BookingSerializer
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from .models import MenuItem,Booking
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+	return render(request, 'index.html', {})
+
+@permission_classes([IsAuthenticated])		
+class MenuItemsView(generics.ListCreateAPIView):
+	queryset = MenuItem.objects.all()
+	serializer_class = MenuItemSerializer
+
+@permission_classes([IsAuthenticated])	
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+	queryset = MenuItem.objects.all()
+	serializer_class = MenuItemSerializer
+
+@permission_classes([IsAuthenticated])	
+class BookingListCreateView(generics.ListCreateAPIView):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+
+@permission_classes([IsAuthenticated])	
+class SingleBookingview(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+
